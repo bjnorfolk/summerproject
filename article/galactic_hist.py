409bd7c9abf4b5fa210a8aa652fb1df606e763a4
bjnorfolk <bjlee7@student.monash.edu>
2018-09-08 10:27:52 +1000

    paper progress

---
+++ article/galactic_hist.py
@@ -0,0 +1,49 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from astropy.table import Table
+from matplotlib import (colors, cm, ticker)
+
+from mpl_utils import mpl_style
+
+plt.style.use(mpl_style)
+
+stars = Table.read("dynamics_complete_test.csv")
+lamosts = Table.read("lamost_gaia_result.fits")
+
+
+s = stars['GAL_LAT']
+l = lamosts['GAL_LAT']
+
+b = np.absolute(s)
+b_l = np.absolute(l)
+s_h, bin_edges = np.histogram(b, 89)
+l_h, bin_edges = np.histogram(b_l, 89)
+div = np.divide(s_h, l_h)
+#sl_h = div
+#sl_h = np.transpose(sl_h)
+
+#grid_slm = np.ma.masked_where(grid_sl < 0.001, grid_sl)
+
+fig, axes = plt.subplots(2, 1)
+axes[0].bar(bin_edges[:-1], s_h, width = 1, facecolor='#FF9309', alpha=0.5)
+axes[0].set_xlim(min(bin_edges), max(bin_edges))
+axes[0].set_xlabel(r'$|b|$ $[^\circ]$')
+axes[0].set_ylabel(r'Number of candidates', 
+	                rotation=90)
+
+axes[1].bar(bin_edges[:-1], div, width = 1, facecolor='#FF9309', alpha=0.5)
+axes[1].set_xlim(min(bin_edges), max(bin_edges))
+axes[1].set_xlabel(r'$|b|$ $[^\circ]$')
+axes[1].set_ylabel(r'Fraction of candidates relative to LAMOST',
+	                 rotation=90)
+
+for ax in axes:
+    ax.xaxis.set_major_locator(ticker.MaxNLocator(6))
+    ax.yaxis.set_major_locator(ticker.MaxNLocator(6))
+
+fig.tight_layout()
+plt.show()
+'''
+fig.savefig('DynamicFigures/'+'histogram'+'.png')
+fig.savefig("DynamicFigures/histogram.pdf", dpi=150)
+'''
